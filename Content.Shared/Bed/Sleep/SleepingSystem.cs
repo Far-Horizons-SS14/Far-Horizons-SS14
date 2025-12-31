38bdd035e8175a647c5bb704a55ea7a5f5979387
Rinary <rinary.super@gmail.com>
2026-01-01 01:51:56 +0200

    Apply suggestions from code review

--- Content.Shared/Bed/Sleep/SleepingSystem.cs
+++ Content.Shared/Bed/Sleep/SleepingSystem.cs
@@ -351,11 +351,8 @@ public sealed partial class SleepingSystem : EntitySystem
 
         /// Starlight
         /// Ensures that people who are SSD cannot be woken up by others.
-        if (TryComp(ent.Owner, out SSDIndicatorComponent? SSDComp))
-        {
-            if (SSDComp.IsSSD)
-                return false;
-        }
+        if (TryComp(ent.Owner, out SSDIndicatorComponent? SSDComp) && SSDComp.IsSSD)
+            return false;
 
         Wake((ent, ent.Comp));
         return true;
