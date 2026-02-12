fbd81f951756849de257eee90cd2a1fff4f52be3
CorruptOmega <207436787+CorruptOmega@users.noreply.github.com>
2026-02-11 20:19:12 -0500

    Adding a NeoSol Admin Mouse, adding comments.

--- Content.Server/_Starlight/Ghost/AdminMouseSystem.cs
+++ Content.Server/_Starlight/Ghost/AdminMouseSystem.cs
@@ -13,7 +13,7 @@ namespace Content.Server._Starlight.Ghost;
 
 public sealed class AdminMouseSystem : EntitySystem
 {
-    [Dependency] private readonly IDiscordLinkManager _playerRoles = default!;
+    [Dependency] private readonly IDiscordLinkManager _playerRoles = default!; /// Far Horizons
     [Dependency] private readonly IAdminManager _admin = default!;
     [Dependency] private readonly PolymorphSystem _polymorphSystem = default!;
     public override void Initialize()
@@ -32,30 +32,50 @@ public sealed class AdminMouseSystem : EntitySystem
 
         if (_admin.IsAdmin(user, true))
         {
-            var adminName = Loc.GetString("admin-verb-text-make-adminmouse");
+            var adminName = Loc.GetString("admin-verb-text-make-adminmouse-nanotrasen"); ///Far Horizons
             Verb admin = new()
             {
                 Text = adminName,
                 Category = VerbCategory.Tricks,
-                Icon = new SpriteSpecifier.Rsi(new ResPath("/Textures/_FarHorizons/Mobs/Animals/mouse.rsi"), "mouse-admin"),
+                Icon = new SpriteSpecifier.Rsi(new ResPath("/Textures/_FarHorizons/Mobs/Animals/mouse.rsi"), "mouse-admin"), ///Far Horizons
                 Act = () =>
                 {
                     _polymorphSystem.PolymorphEntity(user, "AdminMouse");
                 },
                 Impact = LogImpact.Extreme,
-                Message = string.Join(": ", adminName, Loc.GetString("admin-verb-make-adminmouse")),
+/// Far Horizons-Start - Making two separate admin mice
+                Message = string.Join(": ", adminName, Loc.GetString("admin-verb-make-adminmouse-nanotrasen")),
+            };
+            args.Verbs.Add(admin);
+        }
+
+        if (_admin.IsAdmin(user, true))
+        {
+            var adminName = Loc.GetString("admin-verb-text-make-adminmouse-neosol");
+            Verb admin = new()
+            {
+                Text = adminName,
+                Category = VerbCategory.Tricks,
+                Icon = new SpriteSpecifier.Rsi(new ResPath("/Textures/_FarHorizons/Mobs/Animals/mouse.rsi"), "mouse-adminsyndie"),
+                Act = () =>
+                {
+                    _polymorphSystem.PolymorphEntity(user, "AdminMouseSyndi");
+                },
+                Impact = LogImpact.Extreme,
+                Message = string.Join(": ", adminName, Loc.GetString("admin-verb-make-adminmouse-neosol")),
             };
             args.Verbs.Add(admin);
         }
 
         if (_playerRoles.HasPermission(user, AdditionalPermissionsTypes.Mentor))
+/// Far Horizons-End
         {
             var mentorName = Loc.GetString("admin-verb-text-make-mentormouse");
             Verb mentor = new()
             {
                 Text = mentorName,
                 Category = VerbCategory.Tricks,
-                Icon = new SpriteSpecifier.Rsi(new ResPath("/Textures/_FarHorizons/Mobs/Animals/mouse.rsi"), "mouse-mentor"),
+                Icon = new SpriteSpecifier.Rsi(new ResPath("/Textures/_FarHorizons/Mobs/Animals/mouse.rsi"), "mouse-mentor"), ///Far Horizons
                 Act = () =>
                 {
                     _polymorphSystem.PolymorphEntity(user, "MentorMouse");
