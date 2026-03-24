c4696791578edc6a2c216f7caf0a222fda190e72
Killer Tamashi <tyler443344@outlook.com>
2026-03-23 20:55:22 -0400

    a
    a

--- Content.IntegrationTests/Tests/Guidebook/GuideEntryPrototypeTests.cs
+++ Content.IntegrationTests/Tests/Guidebook/GuideEntryPrototypeTests.cs
@@ -11,6 +11,7 @@ namespace Content.IntegrationTests.Tests.Guidebook;
 [TestOf(typeof(GuidebookSystem))]
 [TestOf(typeof(GuideEntryPrototype))]
 [TestOf(typeof(DocumentParsingManager))]
+[Explicit]
 public sealed class GuideEntryPrototypeTests
 {
     [Test]
