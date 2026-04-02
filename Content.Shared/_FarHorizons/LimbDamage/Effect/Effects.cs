84e12caad793f931fde991d52e647b2b66daaafd
princess-gurchi <princess@gurchi.love>
2026-04-02 20:39:17 +0300

    allow limbs to be gibbed outside of body

--- Content.Shared/_FarHorizons/LimbDamage/Effect/Effects.cs
+++ Content.Shared/_FarHorizons/LimbDamage/Effect/Effects.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Content.Shared._FarHorizons.Body;
 using Content.Shared._FarHorizons.LimbDamage.Components;
 using Content.Shared.Body;
 using Robust.Shared.Audio;
@@ -38,22 +39,48 @@ public sealed partial class LimbEffectDestroy : ILimbDamageEffect
     {
         if (limb.Comp.Organ?.Body == null ||
             !entMan.TryGetComponent<BodyComponent>(limb.Comp.Organ.Body.Value, out var body))
-            return;
+            DestroyLimbOutsideOfBody(limb, entMan, protoMan);
+        else
+            DestroyLimbInBody(limb, entMan, protoMan, body);
+    }
 
+    private void DestroyLimbOutsideOfBody(Entity<DamageableLimbComponent> limb, IEntityManager entMan, IPrototypeManager protoMan)
+    {
+        if (!entMan.TryGetComponent<ConnectedOrganComponent>(limb, out var connectedOrgan) ||
+            connectedOrgan.Organs == null)
+            MakeNoiseAndDelete(limb, entMan);
+        else
+        {
+            var connectedOrgans = new List<EntityUid>(connectedOrgan.Organs.ContainedEntities); // A copy because we're about to modify original
+        
+            DropAllConnected(connectedOrgans, entMan);
+            MakeNoiseAndDelete(limb, entMan);
+        }
+    }
+
+    private static void DestroyLimbInBody(Entity<DamageableLimbComponent> limb, IEntityManager entMan, IPrototypeManager protoMan, BodyComponent body)
+    {
         var connectedOrgans = body.Organs?.ContainedEntities.Where(p =>
             entMan.TryGetComponent<OrganComponent>(p, out var organ) &&
-            protoMan.Index(organ.Category)?.ConnectsTo == limb.Comp.Organ.Category).ToList();
-        
-        if (connectedOrgans == null)
-            return;
+            protoMan.Index(organ.Category)?.ConnectsTo == limb.Comp.Organ!.Category).ToList();
+
+        DropAllConnected(connectedOrgans, entMan);
+        MakeNoiseAndDelete(limb, entMan);
+    }
+
+    private static void DropAllConnected(List<EntityUid>? organs, IEntityManager entMan)
+    {
+        if (organs == null || organs.Count == 0) return;
 
         var container = entMan.System<SharedContainerSystem>();
 
-        foreach (var organ in connectedOrgans)
+        foreach (var organ in organs)
             container.TryRemoveFromContainer(organ);
-
-        entMan.System<SharedAudioSystem>().PlayPredicted(_gibSound, limb.Comp.Organ.Body.Value, null);
-        entMan.QueueDeleteEntity(limb);
     }
-        
+
+    private static void MakeNoiseAndDelete(EntityUid target, IEntityManager entMan)
+    {
+        entMan.System<SharedAudioSystem>().PlayPredicted(_gibSound, target, null);
+        entMan.QueueDeleteEntity(target);
+    }
 }
