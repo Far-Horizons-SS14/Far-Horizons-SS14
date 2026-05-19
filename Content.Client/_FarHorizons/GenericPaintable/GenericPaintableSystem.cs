1200d98d2e002f34872109480a0e9953e9723767
Brigader-Blake <blakelapsey@icloud.com>
2026-05-18 23:36:33 -0500

    better naming

--- Content.Client/_FarHorizons/GenericPaintable/GenericPaintableSystem.cs
+++ Content.Client/_FarHorizons/GenericPaintable/GenericPaintableSystem.cs
@@ -1,23 +1,23 @@
-using Content.Shared._FarHorizons.PaintableWall;
+using Content.Shared._FarHorizons.GenericPaintable;
 using Content.Shared.SprayPainter.Prototypes;
 using Robust.Client.GameObjects;
 using Robust.Shared.Prototypes;
 
-namespace Content.Client._FarHorizons.PaintableWall;
+namespace Content.Client._FarHorizons.GenericPaintable;
 
-public sealed class PaintableWallSystem : EntitySystem
+public sealed class GenericPaintableSystem : EntitySystem
 {
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly IComponentFactory _componentFactory = default!;
     [Dependency] private readonly SpriteSystem _sprite = default!;
-    [Dependency] protected readonly SharedAppearanceSystem _appearanceSystem = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearanceSystem = default!;
     public override void Initialize()
     {
         base.Initialize();
-        SubscribeLocalEvent<PaintableWallComponent, AppearanceChangeEvent>(OnAppearanceChange);
+        SubscribeLocalEvent<GenericPaintableComponent, AppearanceChangeEvent>(OnAppearanceChange);
     }
 
-    private void OnAppearanceChange(Entity<PaintableWallComponent> entity, ref AppearanceChangeEvent args)
+    private void OnAppearanceChange(Entity<GenericPaintableComponent> entity, ref AppearanceChangeEvent args)
     {
         if (args.Sprite == null)
             return;
---
+++ Content.Shared/_FarHorizons/GenericPaintable/GenericPaintableComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._FarHorizons.GenericPaintable;
+
+/// <summary>
+/// Generic Component for paintable entities
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class GenericPaintableComponent : Component
+{
+}
