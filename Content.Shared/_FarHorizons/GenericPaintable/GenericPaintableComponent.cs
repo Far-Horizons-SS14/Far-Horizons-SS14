4ab151740446ecf185587f06ba5f8a818572af9a
Brigader-Blake <blakelapsey@icloud.com>
2026-05-19 20:21:01 -0500

    finished walls

--- Content.Shared/_FarHorizons/GenericPaintable/GenericPaintableComponent.cs
+++ Content.Shared/_FarHorizons/GenericPaintable/GenericPaintableComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._FarHorizons.GenericPaintable;
 
 /// <summary>
-/// Generic Component for paintable entities
+/// generic component for paintable entities, should always be used in combination with PaintableComponent
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class GenericPaintableComponent : Component
