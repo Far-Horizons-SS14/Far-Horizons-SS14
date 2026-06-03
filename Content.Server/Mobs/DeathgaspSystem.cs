8c19b057e0972d2e6b8b868a185ed9cae718d6e7
princess-gurchi <princess@gurchi.love>
2026-06-03 20:21:18 +0300

    fix deathgasp with active crit

--- Content.Server/Mobs/DeathgaspSystem.cs
+++ Content.Server/Mobs/DeathgaspSystem.cs
@@ -21,7 +21,8 @@ public sealed class DeathgaspSystem: EntitySystem
     private void OnMobStateChanged(EntityUid uid, DeathgaspComponent component, MobStateChangedEvent args)
     {
         // don't deathgasp if they arent going straight from crit to dead
-        if (args.NewMobState != MobState.Dead || args.OldMobState != MobState.Critical)
+        List<MobState> critStates = [MobState.Critical, MobState.ActiveCritical]; // Far Horizons
+        if (args.NewMobState != MobState.Dead || !critStates.Contains(args.OldMobState))
             return;
 
         Deathgasp(uid, component);
