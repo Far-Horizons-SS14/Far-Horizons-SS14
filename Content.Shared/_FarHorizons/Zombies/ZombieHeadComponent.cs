b045dd6d08a11beff447100e99cb0c5eed2e1384
princess-gurchi <princess@gurchi.love>
2026-09-26 13:53:51 +0300

    nerf monkey

--- Content.Shared/_FarHorizons/Zombies/ZombieHeadComponent.cs
+++ Content.Shared/_FarHorizons/Zombies/ZombieHeadComponent.cs
@@ -5,3 +5,6 @@ public sealed partial class ZombieHeadComponent : Component
 {
     [DataField] public float DeathAt = 200;
 }
+
+[RegisterComponent]
+public sealed partial class LegacyZombieLockComponent : Component; // Component used to "tag" mobs to use legacy zombie system (no limb damage)
